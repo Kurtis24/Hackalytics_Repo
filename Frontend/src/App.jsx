195bade196b2ad7802b65db41428e1a8b3012db6
Brian Su <brians.24.25@gmail.com>
2026-02-21 02:07:15 -0500

    small changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,5 @@
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 export default function App() {
-  return (
-    <div className="bg-gray-50">
-      <Navbar />
-      <Home />
-    </div>
-  )
+  return <Home />
 }
